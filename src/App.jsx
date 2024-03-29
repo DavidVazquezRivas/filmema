2c31b459d7b4cdb7498ae770ae0d8519ee002cc3
David <vazquezrivasdavid@gmail.com>
2024-03-29 04:07:02 +0100

    Early header styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,22 @@ function App() {
 
   return (
     <div className="app">
-      <Route 
-        component={MovieCardList}
-        path='/search/:keyword'
-      />
+      <header>
+        <div className='type-select'>
+          <button>MOVIES</button>
+          <button>SERIES</button>
+        </div> 
+        <form className='searcher'>
+          <input type='text' placeholder='Type title to search'></input>
+          <button type='submit'><i class="fa fa-search"></i></button>
+        </form>
+      </header>
+      <main>
+        <Route 
+          component={MovieCardList}
+          path='/search/:keyword'
+        />
+      </main>
     </ div>
   )
 }
