46e1d88860034391093e9ec782613c7eae4ccc59
David <vazquezrivasdavid@gmail.com>
2024-04-01 01:15:41 +0200

    css class rename

--- src/components/MovieCardList/MovieCardList.jsx
+++ src/components/MovieCardList/MovieCardList.jsx
@@ -15,5 +15,5 @@ export default function MovieCardList({ movies }) {
 		)
 	})
 
-	return <ul className='MovieCardList'>{movieCards}</ul>
+	return <ul className='movie-card-list'>{movieCards}</ul>
 }
